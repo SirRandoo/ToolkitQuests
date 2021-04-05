545b313e79b402c6bf043b5d5176e8b100aee0de
SirRandoo <8146689+SirRandoo@users.noreply.github.com>
2021-04-05 05:04:34 -0500

    Initial commit.

---
+++ Source/ToolkitQuests/TkQuests.cs
@@ -0,0 +1,21 @@
+﻿using JetBrains.Annotations;
+using Verse;
+
+namespace SirRandoo.ToolkitQuests
+{
+    [UsedImplicitly]
+    public class TkQuests : Mod
+    {
+        internal static TkQuests Instance;
+
+        public TkQuests(ModContentPack content) : base(content)
+        {
+            Instance = this;
+        }
+
+        public override string SettingsCategory()
+        {
+            return Content.Name;
+        }
+    }
+}
